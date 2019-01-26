6ebc417057133ca305a7f4eba59c82ca62e18c7a
Lee <L918Sturrock@hotmail.com>
2019-01-26 17:08:32 +0000

    Initial commit

---
+++ DataJumper/Assets/Scripts/GameManager/OutOfBounds.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    public ParticleSystem pickupParticle;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // sound clip?
+        pickupParticle.Play();
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
