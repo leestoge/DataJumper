e0d1d46490ae427b089352b040719779e90f0685
Lee <L918Sturrock@hotmail.com>
2019-01-26 22:49:27 +0000

    Set up a particle to play when the player reaches the goal, implemented slow motion functionality when player reaches the goal

--- DataJumper/Assets/Scripts/World/PayloadDelivery.cs
+++ DataJumper/Assets/Scripts/World/PayloadDelivery.cs
@@ -2,11 +2,18 @@
 
 public class PayloadDelivery : MonoBehaviour
 {
+    public ParticleSystem winnerParticle;
+    public TimeManager timeManager;
+
     void OnTriggerEnter(Collider other)
     {
         // start platform particles
+        winnerParticle.Play();
         // sound?
+
         // slow mo?
+        timeManager.InitiateSlowMotion();
+
         Debug.Log("You win.");
     }
 }
