6df659e56acc6e0d54434b0e0e750ab578efb32b
Lee <L918Sturrock@hotmail.com>
2019-01-26 17:39:02 +0000

    Implemented basic payload delivery script basis, to be updated with the commented functionality.

---
+++ DataJumper/Assets/Scripts/World/PayloadDelivery.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PayloadDelivery : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        // start platform particles
+        // sound?
+        // slow mo?
+        Debug.Log("You win.");
+    }
+}
