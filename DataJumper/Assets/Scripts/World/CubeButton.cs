169f09d3f0b0204ee2b559452f5944fd3f266d91
Lee <L918Sturrock@hotmail.com>
2019-01-29 00:44:56 +0000

    Implemented Friend Cube button functionality, needs ui elements (and possibly sound)

---
+++ DataJumper/Assets/Scripts/World/CubeButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CubeButton : MonoBehaviour
+{
+    public GameObject snapper;
+    public GameObject item;
+    public GameObject replica;
+
+    void OnTriggerEnter(Collider other)
+    {
+        PickUp PickUpHook = item.GetComponent<PickUp>();
+        if (item.CompareTag("FriendCube") && PickUpHook.isHolding && item != null)
+        {            
+            Debug.Log("Friend cube found!");
+            // voiceline?
+            Destroy(item);
+            Instantiate(replica, snapper.transform.position, Quaternion.Euler(new Vector3(0, Random.Range(30, 140), 0)));
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("You need a friend cube!");
+        }
+    }
+}
