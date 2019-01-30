d8546cfb48f7c48c8d1a33b07436bf26eca00d84
Lee <L918Sturrock@hotmail.com>
2019-01-30 16:56:49 +0000

    Set up level (scene) transitions

--- DataJumper/Assets/Scripts/World/PayloadDelivery.cs
+++ DataJumper/Assets/Scripts/World/PayloadDelivery.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PayloadDelivery : MonoBehaviour
 {
     public ParticleSystem winnerParticle;
     public TimeManager timeManager;
 
+    private IEnumerator WaitForSceneLoad()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
     void OnTriggerEnter(Collider other)
     {
         // start platform particles
@@ -15,5 +23,8 @@ public class PayloadDelivery : MonoBehaviour
         timeManager.InitiateSlowMotion();
 
         Debug.Log("You win.");  // Replace with UI element?
+
+        StartCoroutine(WaitForSceneLoad());
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
