620dce96ee3d82288c4e532193f2aa4e42283d84
Lee <L918Sturrock@hotmail.com>
2019-01-30 00:09:30 +0000

    Added script to detect the fact that the FriendCube was delivered to the button (and then do whatever that calls for)

---
+++ DataJumper/Assets/Scripts/World/ButtonAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ButtonAction : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (prefab.CompareTag("FriendCube"))
+        {
+            Debug.Log("Trigger achieved! (Open door, or whatever)");
+        }
+    }
+}
