ef2ff63832054756aa39de16a5f0994cd873019e
Lee <L918Sturrock@hotmail.com>
2019-02-03 15:51:16 +0000

    Updated out of bounds to differentiate between player and Friendcube and reset them accordingly

--- DataJumper/Assets/Scripts/GameManager/OutOfBounds.cs
+++ DataJumper/Assets/Scripts/GameManager/OutOfBounds.cs
@@ -4,12 +4,23 @@ public class OutOfBounds : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
-    public ParticleSystem pickupParticle;
+    [SerializeField] private Transform cube;
+    [SerializeField] private Transform cubeRedeploy;
+    public ParticleSystem respawnParticle;
+    public ParticleSystem cuberespawnParticle;
 
     void OnTriggerEnter(Collider other)
     {
-        // sound clip?
-        pickupParticle.Play();
-        player.transform.position = respawnPoint.transform.position;
+        if (other.CompareTag("Player"))
+        {
+            // sound clip?
+            respawnParticle.Play();
+            player.transform.position = respawnPoint.transform.position;
+        }
+        else if (other.CompareTag("FriendCube"))
+        {
+            cuberespawnParticle.Play();
+            cube.transform.position = cubeRedeploy.transform.position;
+        }
     }
 }
