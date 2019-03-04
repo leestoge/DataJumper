3a4607ff57920be28934587b2cdf2af6a8c0e745
Lee <L918Sturrock@hotmail.com>
2019-03-04 19:31:44 +0000

    Added rocket jumping/rigidbody force functionality

---
+++ DataJumper/Assets/Scripts/Player/Weapon/FireRocket.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FireRocket : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+    public Transform shootPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FindObjectOfType<AudioManager>().Play("RocketFire");
+            Quaternion rocketRot = Quaternion.Euler(shootPoint.rotation.eulerAngles.x + 90, shootPoint.rotation.eulerAngles.y, shootPoint.rotation.eulerAngles.z);
+            GameObject rocket = Instantiate(rocketPrefab, shootPoint.position, rocketRot);
+            Rigidbody rocketRb = rocket.GetComponent<Rigidbody>();
+            rocketRb.velocity = shootPoint.forward * 25f;
+        }
+    }
+}
