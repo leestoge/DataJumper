dd0da5e9e57bb175f04aaeb8392029872ae456a7
Lee <L918Sturrock@hotmail.com>
2019-03-06 03:24:37 +0000

    Removed unecessary code

--- DataJumper/Assets/Scripts/Player/WallJumping.cs
+++ DataJumper/Assets/Scripts/Player/WallJumping.cs
@@ -4,13 +4,8 @@ public class WallJumping : MonoBehaviour
 {
     private CharacterController _controller;
     private PlayerMovement movementRef;
-
-    // help
-    private Vector3 moveVector;
-    private Vector3 lastMove;
-    private float speed = 8f;
-    private float jumpForce = 8f;
-    private float verticalVelocity;
+    private float upwardsThrust = 10f;
+    private float speedGain = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,26 +14,17 @@ public class WallJumping : MonoBehaviour
         movementRef = GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //movementRef.playerVelocity = Vector3.zero;
-        //movementRef.playerVelocity.y = 0;
-        //movementRef.playerVelocity.Normalize();
-        //movementRef.playerVelocity *= speed;
-        //movementRef.playerVelocity.y = verticalVelocity;
-
-    }
-
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (!_controller.isGrounded && hit.normal.y < 0.1f)
         {
             if (Input.GetButtonDown("Jump"))
-            {
+            {            
                 Debug.DrawRay(hit.point, hit.normal, Color.magenta, 1.25f);
-                movementRef.playerVelocity = hit.normal *10f;
-                movementRef.playerVelocity.y = speed;
+                movementRef.playerVelocity = hit.normal * speedGain;
+                movementRef.playerVelocity.y = upwardsThrust;
+                FindObjectOfType<AudioManager>().RandomizePitch("Jump");
+                FindObjectOfType<AudioManager>().Play("Jump");
             }
         }
     }
