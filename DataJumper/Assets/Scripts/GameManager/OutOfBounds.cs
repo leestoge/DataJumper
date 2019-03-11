5cd92625a0f52737690440a7e4fdbbd9574845e6
Lee <L918Sturrock@hotmail.com>
2019-03-11 03:10:38 +0000

    Optimized code and set player to be disabled, so it goes to the camera pan (may be changed)

--- DataJumper/Assets/Scripts/GameManager/OutOfBounds.cs
+++ DataJumper/Assets/Scripts/GameManager/OutOfBounds.cs
@@ -1,21 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class OutOfBounds : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private Transform respawnPoint;
     [SerializeField] private Transform cube;
     [SerializeField] private Transform cubeRedeploy;
     public ParticleSystem respawnParticle;
     public ParticleSystem cuberespawnParticle;
+    private bool isRespawning;
+    private Vector3 respawnPoint;
+    private PlayerMovement movementRef;
+    public float respawnLength;
+    private ToggleTrail trailToggler;
+
+    void Start()
+    {
+        movementRef = player.GetComponent<PlayerMovement>();
+        trailToggler = player.GetComponent<ToggleTrail>();
+        respawnPoint = player.transform.position;
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {
-            FindObjectOfType<AudioManager>().Play("Respawn");
-            respawnParticle.Play();
-            player.transform.position = respawnPoint.transform.position;
+        {           
+            RespawnPlayer();
         }
         else if (other.CompareTag("FriendCube"))
         {
@@ -23,5 +33,31 @@ public class OutOfBounds : MonoBehaviour
             cuberespawnParticle.Play();
             cube.transform.position = cubeRedeploy.transform.position;
         }
+    }
+
+    public void RespawnPlayer()
+    {      
+        if (!isRespawning)
+        {
+            StartCoroutine("RespawnCoroutine");
+        }
+    }
+
+    public IEnumerator RespawnCoroutine()
+    {
+        isRespawning = true;
+        player.gameObject.SetActive(false);
+        // instantiate gore particle at player last position?
+
+        yield return new WaitForSeconds(respawnLength);
+
+        isRespawning = false;
+
+        player.gameObject.SetActive(true);
+        trailToggler.ClearTrail(); // reset player trail
+        movementRef.playerVelocity = Vector3.zero; // cancel out all speed the player has gained
+        player.transform.position = respawnPoint; // respawn them where they started     
+        FindObjectOfType<AudioManager>().Play("Respawn"); // play respawn noise
+        respawnParticle.Play(); // play respawn particle
     }
 }
