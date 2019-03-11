e666b8db55985d8bf491ba47b71acc1a6b20d0f9
Lee <L918Sturrock@hotmail.com>
2019-03-11 03:10:02 +0000

    Experimented with camera panning around level

---
+++ DataJumper/Assets/Scripts/World/CameraPan.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
