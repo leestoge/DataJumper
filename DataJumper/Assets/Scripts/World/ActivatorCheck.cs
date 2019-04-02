0c58bbe154870072ea4b8145f71389f95e86fed0
Lee <L918Sturrock@hotmail.com>
2019-04-02 02:12:15 +0100

    Changed Activator prefab and script to be less likely to cause errors

--- DataJumper/Assets/Scripts/World/ActivatorCheck.cs
+++ DataJumper/Assets/Scripts/World/ActivatorCheck.cs
@@ -3,7 +3,7 @@
 public class ActivatorCheck : MonoBehaviour
 {
     public GameObject snapper; // where to place replica
-    public GameObject item; // Friend Cube
+    public GameObject FriendCube; // Friend Cube
     public GameObject replica; // replica non-interact-able Friend Cube
 
     public GameObject hologram1; // Hologram-like area of button -  WHAT WE ARE CHANGING THE COLOUR OF
@@ -19,29 +19,39 @@ public class ActivatorCheck : MonoBehaviour
         {
             Debug.Log("Moving Platform doesn't seem to be in this level.");
         }
+
+        if (LevelExit == null)
+        {
+            Debug.Log("Level Exit doesn't seem to be in this level.");
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        PickUp PickUpHook = item.GetComponent<PickUp>(); // hook to PickUp.cs script
+        PickUp PickUpHook = FriendCube.GetComponent<PickUp>(); // hook to PickUp.cs script
 
-        if (other.CompareTag("FriendCube") && PickUpHook.isHolding && item != null) // check if FriendCube, the player is holding it, and the FriendCube isn't somehow null.
+        if (other.CompareTag("FriendCube") && PickUpHook.isHolding && FriendCube != null) // check if FriendCube, the player is holding it, and the FriendCube isn't somehow null.
         {            
             Debug.Log("Friend cube found!"); // Replace with UI element?
-            LevelExit.SetActive(true);
+            // voiceline or beep noise
+
+            if (LevelExit != null)
+            {
+                Debug.Log("Level Exit found");
+                LevelExit.SetActive(true);
+            }
 
             if (movingPlatform != null)
             {
                 Debug.Log("Moving Platform found");
                 movingPlatform.SetActive(true);     
             }
-            // voiceline or beep noise
-
-            Destroy(item); // Destroy original Friend Cube
+            
+            Destroy(FriendCube); // Destroy original Friend Cube
             Instantiate(replica, snapper.transform.position, Quaternion.Euler(new Vector3(0, Random.Range(30, -40), 0))); // Create replica (non-interact-able) friend cube above button with a random rotation on Y axis between 30 - 140 degrees.
             hologram1.gameObject.GetComponent<Renderer>().material = activeMaterial; // Change hologram-like area to the active material (green)
             hologram2.gameObject.GetComponent<Renderer>().material = activeMaterial; // Change hologram-like area to the active material (green)
-            gameObject.SetActive(false);
+            gameObject.SetActive(false); // set the trigger to not be active so it doesn't keep checking.
         }
         else
         {
