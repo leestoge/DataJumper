ce20b5e17bfd7e4265149c7080192bab4a2c2ab3
Lee <L918Sturrock@hotmail.com>
2019-04-22 14:19:36 +0100

    Level Name text cycle implemented

---
+++ DataJumper/Assets/Scripts/Text/LevelNameCycle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelNameCycle : MonoBehaviour
+{
+    public float WaitBetween;
+    private Animator FadeOut;
+    List<Animator> _animators;
+
+    void Awake()
+    {
+        FadeOut = gameObject.GetComponent<Animator>();
+        _animators = new List<Animator>(GetComponentsInChildren<Animator>());
+
+        StartCoroutine(DoAnimation());
+    }
+
+    IEnumerator DoAnimation()
+    {
+        while (true)
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetTrigger("DoAnimation");
+                yield return new WaitForSeconds(WaitBetween);
+            }
+            //end
+            FadeOut.SetTrigger("FadeOut");
+        }
+    }
+}
